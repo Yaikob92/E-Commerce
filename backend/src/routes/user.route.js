cc4b9482f72f536eeb1c0b6964ab3a08f1ac56d0
Yaikob/92 <wyaikob@gmail.com>
2026-01-20 21:31:41 +0300

    user routes & controllers added

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  addAddress,
+  addToWishlist,
+  deleteAddress,
+  getAddresses,
+  updateAddress,
+  getWishlist,
+  removeFromWishlist,
+} from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.use(protectRoute);
+
+// address routes
+router.post("/addresses", addAddress);
+router.get("/addresses", getAddresses);
+router.put("/addresses/:addressId", updateAddress);
+router.delete("/addresses/:addressId", deleteAddress);
+
+// wishlist routes
+router.post("/wishlist", addToWishlist);
+router.delete("/wishlist/:productId", removeFromWishlist);
+router.get("/wishlist", getWishlist);
+
+export default router;
